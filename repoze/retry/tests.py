c423056744d332b1fd3cd117cc5e981737e082d2
ivan1233211 <ivanrouman@gmail.com>
2016-10-03 15:58:49 +0300

    Delete tempfile

--- repoze/retry/tests.py
+++ repoze/retry/tests.py
@@ -1,7 +1,11 @@
 import unittest
 
-class CEBase:
+_MINIMAL_HEADERS = [('Content-Type', 'text/plain')]
 
+
+# ConflictError and RetryExeption
+class CEBase:
+    
     def _getConflictError(self):
         from repoze.retry import ConflictError
         return ConflictError
@@ -14,11 +18,17 @@ class CEBase:
 
     RetryException = property(_getRetryException,)
 
-_MINIMAL_HEADERS = [('Content-Type', 'text/plain')]
-
 def _faux_start_response(result, headers, exc_info=None):
     pass
 
+def unwind(result):
+    # we need to close the app iter to shut lint up
+    result2 = list(result)
+    if hasattr(result, 'close'):
+        result.close()
+    return result2
+
+# Tests
 class FactoryTests(unittest.TestCase, CEBase):
     _dummy_start_response_result = None
     
@@ -34,31 +44,43 @@ class FactoryTests(unittest.TestCase, CEBase):
 
     def _makeEnv(self, **kw):
         return {}
+    
+    def _makeEnvWithErrorsStream(self, **kw):
+        try:
+            from StringIO import StringIO
+        except ImportError: #pragma NO COVER Py3k
+            from io import StringIO
+        env = self._makeEnv(**kw)
+        env['wsgi.errors'] = StringIO()
+        return env
         
-    def test_conflict_raised_start_response_called(self):
+    def test_retry_500_server(self):
         application = DummyApplication(conflicts=5, call_start_response=True)
-        retry = self._makeOne(application, tries=4,
-                              retryable=(self.ConflictError,))
+        retry = self._makeOne(application, tries=3, retryable=(self.ConflictError,))
         self.assertRaises(self.ConflictError,
                           retry, self._makeEnv(), self._dummy_start_response)
-        self.assertEqual(application.called, 4)
+        self.assertEqual(application.called, 3)
         self.assertEqual(self._dummy_start_response_result,
                          ('409 Conflict', _MINIMAL_HEADERS, None))
         
-class Retryable(Exception):
-    pass
-
-class AnotherRetryable(Exception):
-    pass
-
+    def test_200_OK_called(self):
+        application = DummyApplication(conflicts=0, call_start_response=True, status = "200 OK")
+        retry = self._makeOne(application, tries=3, retryable=(self.ConflictError,))
+        retry(self._makeEnv(), self._dummy_start_response)
+        self.assertEqual(application.called, 0)
+        self.assertEqual(self._dummy_start_response_result,
+                         ('200 OK', _MINIMAL_HEADERS, None))
+    
+    
+# Test server 
 class DummyApplication(CEBase):
     iter_factory = list
 
-    def __init__(self, conflicts, call_start_response=False,
-                 exception=None):
+    def __init__(self, conflicts, call_start_response=False, exception=None, status = "500 Internal Server Error"):
         self.called = 0
         self.conflicts = conflicts
         self.call_start_response = call_start_response
+        self.status = status
         if exception is None:
             exception = self.ConflictError
         self.exception = exception
@@ -66,7 +88,7 @@ class DummyApplication(CEBase):
 
     def __call__(self, environ, start_response):
         if self.call_start_response:
-            start_response('500 Internal Server Error', _MINIMAL_HEADERS)
+            start_response(self.status, _MINIMAL_HEADERS)
         if self.called < self.conflicts:
             self.called += 1
             raise self.exception
