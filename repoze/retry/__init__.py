84aef736e7d5f135e3e0cbaf8c1a13b8025a7bf4
ivan1233211 <ivanrouman@gmail.com>
2016-10-03 17:24:26 +0300

    clear

--- repoze/retry/__init__.py
+++ repoze/retry/__init__.py
@@ -1,53 +1,31 @@
-# repoze retry-on-conflict-error behavior
-import itertools
-import socket
-from tempfile import TemporaryFile
 import traceback
 from io import BytesIO
 
-# Avoid hard dependency on ZODB.
 try:
     from ZODB.POSException import ConflictError
 except ImportError:
     class ConflictError(Exception):
         pass
 
-# Avoid hard dependency on Zope2.
-try:
-    from ZPublisher.Publish import Retry as RetryException
-except ImportError:
-    class RetryException(Exception):
-        pass
-
-
 class Retry:
-    def __init__(self, application, tries, retryable=None):
+    def __init__(self, application, tries, retryable=ConflictError):
         
         self.application = application
         self.tries = tries
-
-        if retryable is None:
-            retryable = (ConflictError, RetryException,)
-
-        if not isinstance(retryable, (list, tuple)):
-            retryable = [retryable]
-
         self.retryable = tuple(retryable)
 
     def __call__(self, environ, start_response):
         catch_response = []
-        written = []
         original_wsgi_input = environ.get('wsgi.input')
 
         def replace_start_response(status, headers, exc_info=None):
             catch_response[:] = [status, headers, exc_info]
-            return written.append
 
         count = 0
         while 1:
             try:
                 app_iter = self.application(environ, replace_start_response)
-            except self.retryable as e:
+            except ConflictError:
                 count += 1
                 if count < self.tries:
                     catch_response[:] = []
@@ -55,27 +33,13 @@ class Retry:
                 if catch_response:
                     if catch_response[0] == "500 Internal Server Error":
                         catch_response[0] = "409 Conflict"
-                        start_response(*catch_response)
-                    else:
-                        start_response(*catch_response)
+                    start_response(*catch_response)
                 raise
             else:
-                if catch_response:
-                    start_response(*catch_response)
-                else:
-                    if hasattr(app_iter, 'close'):
-                        app_iter.close()
-                    raise AssertionError('app must call start_response before '
-                                         'returning')
-                return close_when_done_generator(written, app_iter)
-
-def close_when_done_generator(written, app_iter):
-    try:
-        for chunk in itertools.chain(written, app_iter):
-            yield chunk
-    finally:
-        if hasattr(app_iter, 'close'):
-            app_iter.close()
+                start_response(*catch_response)
+                if hasattr(app_iter, 'close'):
+                    app_iter.close()
+                return 
 
 def make_retry(app, global_conf, **local_conf):
     from pkg_resources import EntryPoint
